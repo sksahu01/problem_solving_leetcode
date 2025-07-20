cf1c4a098c17d47572b2eb2a0f8dc583709e7825
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-07-20 22:50:31 +0530

    Time: 0 ms (100.00%) Space: 9.5 MB (82.73%)

---
+++ 0401-binary-watch/binary-watch.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                // Count number of 1's in binary representation - population count
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    result.push_back(to_string(h) + ":" + (m < 10 ? "0" : "") + to_string(m));
+                }
+            }
+        }
+        return result;
+    }
+};
