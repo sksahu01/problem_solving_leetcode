fca862bdb1d1cc7d8d69bb22d7bcd3e13586eadd
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-08-11 23:51:27 +0530

    Time: 0 ms (100.00%) Space: 8.1 MB (11.70%)

---
+++ 1289-day-of-the-week/day-of-the-week.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        vector<string> dayy={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        vector<int> daysOfMonth={0,31,28,31,30,31,30,31,31,30,31,30,31};
+        if(leapYear(year)){
+            daysOfMonth[2]=29;
+        }
+        int daysOfYear = 4;
+
+        for(int i=1971;i<year;i++){
+            if(leapYear(i)){
+                daysOfYear+=366;
+            }else{
+                daysOfYear+=365;
+            }
+        }
+        for(int i=1;i<month;i++){
+            daysOfYear+=daysOfMonth[i];
+        }
+        daysOfYear+=day;
+        return dayy[daysOfYear % 7];
+    }
+
+    bool leapYear(int year){
+        if((year %4==0 && year % 100!=0) || (year % 400==0))
+            return true;
+        return false;
+    }
+};
