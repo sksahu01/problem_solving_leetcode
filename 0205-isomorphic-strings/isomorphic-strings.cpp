34c81952de6c07ed60d492d5837671b21aa5a908
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-08-17 23:54:01 +0530

    Time: 3 ms (43.98%) Space: 9.4 MB (58.06%)

---
+++ 0205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, char> mapST;
+        unordered_map<char, char> mapTS;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if (mapST.count(c1) && mapST[c1] != c2) return false;
+            if (mapTS.count(c2) && mapTS[c2] != c1) return false;
+
+            mapST[c1] = c2;
+            mapTS[c2] = c1;
+        }
+
+        return true;
+    }
+};
