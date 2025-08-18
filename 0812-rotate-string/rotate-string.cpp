79ea653f0893dad520c3176b32d0680e3868585f
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-08-18 23:45:17 +0530

    Time: 0 ms (100.00%) Space: 8 MB (97.13%)

--- 0812-rotate-string/rotate-string.cpp
+++ 0812-rotate-string/rotate-string.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size()) return false;
-        int len=s.size();
-        for(int i=0;i<s.size();i++){
-            string temp=s;
-            if(temp.substr(len-i)+temp.substr(0,len-i)==goal) return true;
-        }
-        return false;
+        string doubled = s + s;
+        return doubled.find(goal) != string::npos;
     }
 };
