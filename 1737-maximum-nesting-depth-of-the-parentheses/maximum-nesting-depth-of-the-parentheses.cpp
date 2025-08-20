cd1ce19e3ca20084d04ccee378a7defe1410d959
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-08-20 23:28:25 +0530

    Time: 0 ms (100.00%) Space: 8.5 MB (25.88%)

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0;
+        int maxDepth=0;
+        for(char c:s){
+            if(c=='('){
+                depth++;
+                maxDepth=max(maxDepth,depth);
+            }
+            if(c==')') depth--;
+
+        }
+        return maxDepth;
+    }
+};
