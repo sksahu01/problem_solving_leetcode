4c7f7838011dc14f332c012c51721261c2f5f0e7
SURAJ KUMAR SAHU <118847989+sksahu01@users.noreply.github.com>
2025-08-26 23:51:49 +0530

    Time: 0 ms (100.00%) Space: 121.9 MB (50.67%)

---
+++ 0234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    // Helper function to reverse a linked list
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* nextNode = head->next;
+            head->next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // 1. Find middle using fast & slow pointers
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse second half
+        slow->next = reverseList(slow->next);
+        ListNode* secondHalf = slow->next;
+
+        // 3. Compare first half and second half
+        ListNode* firstHalf = head;
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) return false;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        // (Optional) Restore list
+        // slow->next = reverseList(slow->next);
+
+        return true;
+    }
+};
